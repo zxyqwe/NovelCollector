6bb87eb0e0e25780262147c17fc7586ea923fb81
zhangxiaoyi <zhangxiaoyi@kuaishou.com>
2022-03-21 11:24:09 +0800

    add epub gen

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
-import re
 import logging
 import argparse
 import requests as rq
+from ebooklib import epub
 from pathlib import Path
 from urllib.request import url2pathname
 from bs4 import BeautifulSoup as soup
@@ -39,6 +39,7 @@ def check_novel(main_url):
         download_file(cache_file, main_url)
     with cache_file.open() as f:
         page = soup(f, "lxml")
+    page_title = page.title.get_text()
 
     # for test, we want get format like:
     # <a href="\d*.html" title=".*">
@@ -52,6 +53,7 @@ def check_novel(main_url):
     root_path = url2pathname(main_url)
     logger.info(f"Root Path: {root_path}")
 
+    ch_list = []
     for idx in chapter_path:
         join_url = root_path + idx
         cache_file = CACHE_DIR / get_pathname_from_url(join_url)
@@ -59,8 +61,52 @@ def check_novel(main_url):
             download_file(cache_file, join_url)
         with cache_file.open() as f:
             page = soup(f, "lxml")
-        print("\n\t".join(page.stripped_strings))
-        exit()
+        title = page.title.get_text()
+        main_content = page.find(id="content")
+
+        # write one chapter
+        tmp_ch = epub.EpubHtml(title=title, file_name=idx)
+        tmp_ch.set_content(f"<h1>{title}</h1>" + str(main_content))
+        ch_list.append(tmp_ch)
+
+        logger.info(f"Handle: {cache_file}")
+
+    book = epub.EpubBook()
+    book.set_title(page_title)
+    book.set_language('zh')
+    for idx in ch_list:
+        book.add_item(idx)
+    nav_css = epub.EpubItem(uid="style_nav", file_name="style/nav.css", media_type="text/css", content='''
+@namespace epub "http://www.idpf.org/2007/ops";
+body {
+    font-family: Cambria, Liberation Serif, Bitstream Vera Serif, Georgia, Times, Times New Roman, serif;
+}
+h2 {
+     text-align: left;
+     text-transform: uppercase;
+     font-weight: 200;
+}
+ol {
+        list-style-type: none;
+}
+ol > li:first-child {
+        margin-top: 0.3em;
+}
+nav[epub|type~='toc'] > ol > li > ol  {
+    list-style-type:square;
+}
+nav[epub|type~='toc'] > ol > li > ol > li {
+        margin-top: 0.3em;
+}
+''')
+    book.add_item(nav_css)
+    book.toc = ch_list
+    book.spine = ['nav', *ch_list]
+    book.add_item(epub.EpubNcx())
+    book.add_item(epub.EpubNav())
+    output_file = cache_file.with_suffix(".epub")
+    epub.write_epub(output_file, book)
+    logger.info(f"Generate: {output_file}")
 
 
 def set_log_if_start_from_this_file():
